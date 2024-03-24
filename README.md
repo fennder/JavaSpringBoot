39114f34990953e07b49796359b787bd21e11cd3
Edir Pereira (Brasília-DF) <fennder@gmail.com>
2024-03-23 23:18:24 -0300

    Create CRUD API RestFull and increment dependency hateoas linked products

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-### Desenvolvimento de aplicação Java Spring Boot Rest
+# Desenvolvimento de aplicação Java Spring Boot RestFull
 
-'''
-MYSQL
-'''
+## Primeiramente no VSCODE deve-se instalar o pluguin para supore do Spring boot
+
+## MYSQL comando docker para criar uma máquina servidora de Banco de dados
+### Acesse para mais informações [Docker MySql](https://gasparbarancelli.com/post/passo-a-passo-para-subir-um-banco-de-dados-mysql-no-docker)
+
+```
+docker run -d --name mysql -e MYSQL_ROOT_PASSWORD=<sua senha> -e MYSQL_USER=usuario -e MYSQL_PASSWORD=<senha> -e MYSQL_DATABASE=banco-de-dados -v /caminho/para/o/dire
+```
--- src/main/java/br/dev/abrindoportas/appweb/controllers/ProductController.java
+++ src/main/java/br/dev/abrindoportas/appweb/controllers/ProductController.java
@@ -1,11 +1,27 @@
 package br.dev.abrindoportas.appweb.controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.dev.abrindoportas.appweb.dtos.ProductRecordDto;
+import br.dev.abrindoportas.appweb.models.ProductModel;
 import br.dev.abrindoportas.appweb.repositories.ProductRepository;
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
@@ -15,11 +31,48 @@ public class ProductController {
   ProductRepository productRepository;
 
   @PostMapping("/products")
-  public String postMethodName(@RequestBody String entity) {
-      //TODO: process POST request
-      
-      return entity;
+  public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+    //TODO: process POST request
+    var productModel = new ProductModel();   
+    BeanUtils.copyProperties(productRecordDto, productModel);   
+    return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
   }
   
+  @GetMapping("/products")
+  public ResponseEntity<List<ProductModel>> getAllProducts() {
+    return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+  }
+  
+  @GetMapping("/products/{id}")
+  public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id) {
+    Optional<ProductModel> productO = productRepository.findById(id);
+    if (productO.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+    }
+    return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+  }
 
+  @PutMapping("/products/{id}")
+  public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id, 
+                                              @RequestBody  @Valid ProductRecordDto productRecordDto) {
+      //TODO: process PUT request
+      Optional<ProductModel> productO = productRepository.findById(id);
+      if (productO.isEmpty()) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+      }
+      var productModel = productO.get();
+      BeanUtils.copyProperties(productRecordDto, productModel);
+      return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+  }
+
+  @DeleteMapping("/products/{id}")
+  public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id){
+    Optional<ProductModel> productO = productRepository.findById(id);
+    if (productO.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+    }   
+    productRepository.delete(productO.get());
+    return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully");
+  }
+  
 }
